2d3a104855c7bb73d46705ec1de05c32299e2202
hery2006 <andrianariveloherimanjato@gmail.com>
2025-10-23 09:20:35 +0300

    Ignorer conception

--- conception/testliste.py
+++
@@ -1,9 +0,0 @@
-liste = list(set([None if x == 8 or x == 2 or x == 5 else x for x in range(12)]))
-
-def Pop_None(liste):
-    for num,valeur in enumerate(liste):
-        if valeur == None:
-            liste.pop(num)
-    return liste
-
-print(Pop_None(liste=liste))
